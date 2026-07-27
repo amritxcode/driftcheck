a6ba960183c72144b17fe00a6c8c02d960b9bf7d
Amrit Ray <amritray43@gmail.com>
2026-07-27 20:45:18 +0530

    Add: get_latest_nav function to fetch live NAV from url

--- driftcheck.py
+++ driftcheck.py
@@ -1,7 +1,7 @@
 import csv
 import os
 import sys
-import pprint
+import requests
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 CSV_PATH = os.path.join(BASE_DIR, "data", "holdings.csv")
@@ -20,3 +20,15 @@ def read_holdings():
     except FileNotFoundError:
         print(f"Error: could not find holdings file at {CSV_PATH}")
         sys.exit(1)
+
+def get_latest_nav(scheme_code):
+    url = f"https://api.mfapi.in/mf/{scheme_code}"
+
+    response = requests.get(url)
+    data = response.json()
+
+    latest_entry = data["data"][0]["nav"]
+    nav = float(latest_entry)
+    return nav
+
+print(get_latest_nav(122639))
